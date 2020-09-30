f38a57177d01a8db5381ea456aa080c4199c32da
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-09-30 16:29:39 +0100

    Add exercise 2

---
+++ modulo-2/leccion-01/modulo-2-leccion-01-ejercicio-02-arreglando-errores/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+document.querySelector("h1").innerHTML = "Jesus Ferrer Jimeno";
+
+/* La declaración de constantes no admite cambio de valores
+const homeAddress = "Jesus Ferrer Jimeno, 14";
+homeAddress = "Cirilo Moreno, 6"; */
+
+// Los valores se pueden cambiar utilizando variables en su lugar
+let homeAddress;
+homeAddress = "Jesus Ferrer Jimeno, 14";
+homeAddress = "Cirilo Moreno, 6";
