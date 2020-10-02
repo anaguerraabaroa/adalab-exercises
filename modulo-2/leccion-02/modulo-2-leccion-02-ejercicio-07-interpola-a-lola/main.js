fee9145f16a78171e2c871dd1fa2eaa67234f4a9
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-02 08:46:29 +0100

    Update exercises

--- modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-07-interpola-a-lola/main.js
+++ modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-07-interpola-a-lola/main.js
@@ -4,5 +4,5 @@ const greetingInner1 = "Hola Ana, ";
 const greetingInner2 = "encantada de conocerte";
 greeting.innerHTML = greetingInner1 + greetingInner2;
 console.log(
-  `Esto es un mensaje de bienvenida: ${greetingInner1 + greetingInner2}`
+  `Esto es un mensaje de bienvenida: ${greetingInner1} ${greetingInner2}`
 );
