754daa3ef22ab4e9bfccdd24b6de89f46224db0e
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-03 00:21:44 +0100

    Add exercises module 2 lesson 3

--- modulo-2/leccion-03/modulo-2-leccion-03-ejercicio-03-completa-las-condiciones/main.js
+++ modulo-2/leccion-03/modulo-2-leccion-03-ejercicio-03-completa-las-condiciones/main.js
@@ -7,7 +7,7 @@ if (numberValue === 0) {
   console.log("El número es 0");
 } else if (numberValue < 0) {
   console.log("El número es negativo");
-} else if (numberValue + 2 > 13 && numberValue <= 20) {
+} else if (numberValue + 2 > 13 && numberValue + 2 <= 20) {
   console.log("El número más 2 es mayor que 13 pero menor o igual que 20");
 } else if (numberValue > 20 && numberValue < 50) {
   console.log("El número es mayor que 20 pero menor que 50");
