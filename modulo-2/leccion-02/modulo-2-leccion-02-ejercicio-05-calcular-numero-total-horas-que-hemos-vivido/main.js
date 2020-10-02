f13fbe814f2be51983fcb9c4224dec079c3d3ecc
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-02 10:17:03 +0100

    Update exercises

--- modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-05-calcular-numero-total-horas-que-hemos-vivido/main.js
+++ modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-05-calcular-numero-total-horas-que-hemos-vivido/main.js
@@ -4,9 +4,10 @@ const totalHours = document.querySelector(".sum");
 const hoursPerDay = 24;
 const daysPerYear = 365;
 const age = 38;
-totalHours.innerHTML = hoursPerDay * daysPerYear * age;
+const bisi = (age / 4) * hoursPerDay;
+totalHours.innerHTML = hoursPerDay * daysPerYear * age + bisi;
 console.log(totalHours);
 
 /* Mensaje que aparece en la consola al loguear las constantes
-console.log(hoursPerDay * daysPerYear * age);
+console.log(hoursPerDay * daysPerYear * age + bisi);
 332880 */
--- modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-10-recogiendo-numeros-de-HTML/main.js
+++ modulo-2/leccion-02/modulo-2-leccion-02-ejercicio-10-recogiendo-numeros-de-HTML/main.js
@@ -1,5 +1,6 @@
 "use strict";
 const userAge = document.querySelector(".user_age");
 const totalHours = 24 * 365 * parseInt(userAge.innerHTML);
-userAge.innerHTML = totalHours;
+const body = document.querySelector("body");
+body.innerHTML = body.innerHTML + `<p> ${totalHours}</p>`;
 console.log(totalHours);
