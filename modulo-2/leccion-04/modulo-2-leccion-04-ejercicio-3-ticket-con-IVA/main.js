118a20461bf492a3d25f6d7e765c586db3b4ef0c
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-06 09:06:54 +0100

    Update exercise 4 module 2 lesson 4

--- modulo-2/leccion-04/modulo-2-leccion-04-ejercicio-3-ticket-con-IVA/main.js
+++ modulo-2/leccion-04/modulo-2-leccion-04-ejercicio-3-ticket-con-IVA/main.js
@@ -1,9 +1,21 @@
 "use strict";
 
-function priceIva(price) {
+function getIva(price) {
   const iva = price * 0.21;
   const total = price + iva;
   console.log(`Precio sin IVA: ${price}; IVA: ${iva}; Total: ${total}`);
 }
 
-let totalPrice = priceIva(100);
+getIva(100);
+
+// ("use strict");
+
+// function getIva(price) {
+//   const iva = price * 0.21;
+//   const total = price + iva;
+//   const texto = `Precio sin IVA: ${price}; IVA: ${iva}; Total: ${total}`;
+//   return texto;
+// }
+
+// const paragraph = document.querySelector(".js-text");
+// paragraph.innerHTML = getIva(100);
