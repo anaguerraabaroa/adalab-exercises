ccdfd07e032d4d399078c4a4b1914b39adf98ebd
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-14 17:10:59 +0100

    Add exercise 1 module 2 lesson 9

---
+++ modulo-2/lección-09/modulo-2-leccion-09-ejercicio-1-numeritos/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Declaramos un array vacío para completar sus elementos con el método array.push como pide el enunciado del ejercicio
+const array = [];
+
+// El bucle genera los elementos que van a conformar el array y que incluimos dentro del mismo con el método array.push
+// Metemos el bucle dentro de la función para que al invocarla nos devuelva el array con sus elementos dentro
+function get100Numbers() {
+  for (let i = 1; i < 101; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+get100Numbers();
+
+// Logueamos para confirmar en consola que el array está completo
+console.log(array);
