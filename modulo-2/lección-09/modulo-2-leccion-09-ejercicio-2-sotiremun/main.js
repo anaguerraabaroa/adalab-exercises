cb7c0d3e26aa80e424de71bb40215aec8ff6bcd9
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-15 09:19:39 +0100

    Update exercises lesson 9

--- modulo-2/lección-09/modulo-2-leccion-09-ejercicio-2-sotiremun/main.js
+++ modulo-2/lección-09/modulo-2-leccion-09-ejercicio-2-sotiremun/main.js
@@ -4,7 +4,7 @@
 const arrayNumbers = [];
 
 // El bucle genera los elementos que van a conformar el array y que incluimos dentro del mismo con el método array.push
-// El enunciado indica que el array debe contener números de 1 a 100, por lo que el valor inicial será 1 y la condición debe cumplirse 100 veces
+// El enunciado indica que el array debe contener números de 1 a 100, por lo que el valor inicial será 1 y la condición debe cumplirse siempre que los números sean menor o igual a 100 y el recorrido pase por todos los elementos del array
 // Metemos el bucle dentro de la función para que al invocarla nos devuelva el array con sus elementos dentro
 function get100Numbers() {
   for (let i = 1; i <= 100; i++) {
