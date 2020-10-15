7972b730e0c004003c48a4f235fb51312b2f3f92
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-15 08:19:11 +0100

    Update exercise 1 module 2 lesson 9

--- modulo-2/lección-09/modulo-2-leccion-09-ejercicio-1-numeritos/main.js
+++ modulo-2/lección-09/modulo-2-leccion-09-ejercicio-1-numeritos/main.js
@@ -1,16 +1,16 @@
 "use strict";
 
 // Declaramos un array vacío para completar sus elementos con el método array.push como pide el enunciado del ejercicio
-const array = [];
+const arrayNumbers = [];
 
 // El bucle genera los elementos que van a conformar el array y que incluimos dentro del mismo con el método array.push
 // El enunciado indica que el array debe contener números de 1 a 100, por lo que el valor inicial será 1 y la condición debe cumplirse 100 veces
 // Metemos el bucle dentro de la función para que al invocarla nos devuelva el array con sus elementos dentro
 function get100Numbers() {
   for (let i = 1; i <= 100; i++) {
-    array.push(i);
+    arrayNumbers.push(i);
   }
-  return array;
+  return arrayNumbers;
 }
 
 // Invocamos a la función
