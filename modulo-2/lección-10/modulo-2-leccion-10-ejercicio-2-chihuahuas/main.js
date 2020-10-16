5697904ef458bdc75b8340a8e7e0f362911d477c
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-16 18:50:03 +0100

    Update exercise 2 module 2 lesson 10

--- modulo-2/lección-10/modulo-2-leccion-10-ejercicio-2-chihuahuas/main.js
+++ modulo-2/lección-10/modulo-2-leccion-10-ejercicio-2-chihuahuas/main.js
@@ -1,13 +1,20 @@
 "use strict";
 
+// Respuesta a
+
+const btn = document.querySelector(".js-dog");
+const img = document.querySelector(".img");
+const btn2 = document.querySelector(".js-dog2");
+
 function getDogImage() {
   fetch("https://dog.ceo/api/breed/chihuahua/images/random")
     .then((response) => response.json())
     .then((data) => {
-      const img = document.querySelector("img");
+      document.querySelector(".img");
       img.src = data.message;
       img.alt = "Un perro";
     });
 }
-const btn = document.querySelector(".js-dog");
+
 btn.addEventListener("click", getDogImage);
+btn2.addEventListener("click", getDogImage);
