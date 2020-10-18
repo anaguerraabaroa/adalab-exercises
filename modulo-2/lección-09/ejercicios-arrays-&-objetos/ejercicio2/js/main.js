2a54bbaac8e86324b425f5dfb8d72b1ca855e623
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-18 21:21:06 +0100

    Add arrays extra exercises

---
+++ modulo-2/lección-09/ejercicios-arrays-&-objetos/ejercicio2/js/main.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const promos = [
+  {
+    promo: "A",
+    name: "Ada",
+    students: [
+      {
+        id: "id-1",
+        name: "Sofía",
+        age: 20,
+      },
+      {
+        id: "id-2",
+        name: "María",
+        age: 21,
+      },
+      {
+        id: "id-3",
+        name: "Lucía",
+        age: 22,
+      },
+    ],
+  },
+  {
+    promo: "B",
+    name: "Borg",
+    students: [
+      {
+        id: "id-4",
+        name: "Julia",
+        age: 23,
+      },
+      {
+        id: "id-5",
+        name: "Tania",
+        age: 24,
+      },
+      {
+        id: "id-6",
+        name: "Alaia",
+        age: 25,
+      },
+    ],
+  },
+  {
+    promo: "C",
+    name: "Clarke",
+    students: [
+      {
+        id: "id-7",
+        name: "Lidia",
+        age: 26,
+      },
+      {
+        id: "id-8",
+        name: "Celia",
+        age: 27,
+      },
+      {
+        id: "id-9",
+        name: "Nadia",
+        age: 28,
+      },
+    ],
+  },
+];
+
+const studentsWorkingInGoogle = ["id-2", "id-3", "id-5", "id-9"];
+
+const divElement = document.querySelector(".js-result");
+
+function getPromoInfo() {
+  let promoInfo = "";
+  for (let i = 0; i < promos.length; i++) {
+    promoInfo += `<li><p>Nombre: ${promos[i].name}</p><p>Promo: ${promos[i].promo}</p></li>`;
+  }
+  divElement.innerHTML = `<ul>${promoInfo}</ul>`;
+}
+
+getPromoInfo();
