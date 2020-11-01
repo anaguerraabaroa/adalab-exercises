f0f31af1352fc0a5d17eebc7edb6b66f42f4490e
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-11-01 16:44:33 +0000

    Add exercise 2 module 3 lesson 2

---
+++ modulo-3/lección-02/modulo-3-leccion-02-ejercicio-02/mediacard/src/App.js
@@ -0,0 +1,42 @@
+import avatar from "./avatar.jpg";
+import "./App.css";
+
+function App() {
+  const header = (
+    <header className="App-header">
+      <img src={avatar} className="App-avatar" alt="Avatar" />
+      <div className="App-title">
+        <h1 className="App-name">Ana Guerra</h1>
+        <h2 className="App-date">Domingo 1 de noviembre de 2020</h2>
+      </div>
+    </header>
+  );
+  const paragraph = (
+    <p className="App-text">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  );
+  const small = (
+    <small className="App-small">
+      <p className="App-info">Leer más...</p>
+      <div className="App-likes">
+        <p className="App-likesNumber">20</p>
+        <i className="App-icon fas fa-heart"></i>
+      </div>
+    </small>
+  );
+  const appRoot = (
+    <div className="App">
+      {header} {paragraph} {small}
+    </div>
+  );
+  return appRoot;
+}
+
+export default App;
