cc00d09bcaf7409679c63d7b573087ce3eebdac4
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-11-04 20:34:49 +0000

    Add exercises 3 and 4 module 3 lesson 4

---
+++ modulo-3/lección-04/modulo-3-leccion-04-ejercicio-03/pintar-listas/src/components/ItemList.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Item from "../components/Item";
+
+const arrayOfItems = [
+  {
+    name: "Cereales con chocolate",
+    description: "Cereales rellenos de chocolate",
+    quantity: 2,
+    category: "Cereales",
+    price: 5,
+  },
+  {
+    name: "Hamburguesa con queso",
+    description: "Hamburguesa rica y saludable",
+    quantity: 1,
+    category: "Fast-food",
+    price: 15,
+  },
+  {
+    name: "Agua mineral",
+    description: "Agua de un charco del Himalaya",
+    quantity: 2,
+    category: "Bebida",
+    price: 5,
+  },
+  {
+    name: "Galletas de chocolate",
+    //description: "Galletas rellenas de chocolate",
+    quantity: 3,
+    category: "Galletas",
+    price: 7,
+  },
+];
+
+const listOfItems = arrayOfItems.map((item, index) => {
+  Item.defaultProps = {
+    description: "No hay descripción",
+  };
+  return (
+    <Item
+      key={index}
+      name={item.name}
+      description={item.description}
+      quantity={item.quantity}
+      category={item.quantity}
+      price={item.price}
+    />
+  );
+});
+
+class ItemList extends React.Component {
+  render() {
+    return <ul className="item-list">{listOfItems}</ul>;
+  }
+}
+
+export default ItemList;
---
+++ modulo-3/lección-04/modulo-3-leccion-04-ejercicio-04/pintar-listas/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemList from "../components/ItemList";
+import "../stylesheets/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <ItemList />
+    </div>
+  );
+}
+
+export default App;
