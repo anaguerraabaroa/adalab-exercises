b6dced82ae1af92aea4cb022e61f993e48febfcd
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-01-14 18:56:01 +0000

    Add module 4 lesson 3 exercise 1

---
+++ modulo-4/lección-03/modulo-4-leccion-03-ejercicio-01-mi-propia-libreria-math/index.js
@@ -0,0 +1,14 @@
+// Import module
+const math = require("./math.js");
+
+// Variables
+const num1 = 4;
+const num2 = 2;
+
+// Functions
+const sum = math.sum(num1, num2);
+const substract = math.substract(num1, num2);
+
+// Results
+console.log("The sum result is:", sum);
+console.log("The substract result is:", substract);
---
+++ modulo-4/lección-03/modulo-4-leccion-03-ejercicio-01-mi-propia-libreria-math/math.js
@@ -0,0 +1,15 @@
+// Handle sum
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+// Handle substract
+const substract = (num1, num2) => {
+  return num1 - num2;
+};
+
+// Export module
+module.exports = {
+  sum: sum,
+  substract: substract,
+};
