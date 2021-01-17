725a2c821c01503d0d37c5370df3bab242b274f7
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-01-17 15:12:41 +0000

    Add module 4 lesson 2.3 exercise 2

---
+++ modulo-4/leccion-2.3/modulo-4-leccion-2.4-ejercicio-02-filtrar-usuarias-por-nombre-email/src/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+
+// create server
+const server = express();
+
+// set express middleware
+server.use(express.json());
+server.use(cors());
+
+// init express aplication
+const serverPort = 3000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// static server
+const staticServerPath = "./public";
+server.use(express.static(staticServerPath));
+
+// users
+
+const users = []; // fake users data base
+const filteredUsers = [];
+
+// api endpoints
+
+server.post("/user", (req, res) => {
+  console.log("Query params:", req.query);
+  console.log("Query param userName:", req.query.userName);
+  console.log("Query param userEmail:", req.query.userEmail);
+  console.log("Query param filterName:", req.query.filterName);
+
+  // add new user to daba base
+  users.push({
+    name: req.query.userName,
+    email: req.query.userEmail,
+  });
+  res.json({
+    result: "User created",
+  });
+});
+
+server.get("/users", (req, res) => {
+  const name = req.query.filterName;
+  const email = req.query.filterEmail;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name.includes(name) && users[i].email.includes(email)) {
+      filteredUsers.push({
+        name: users[i].name,
+        email: users[i].email,
+      });
+    } else {
+      filteredUsers;
+    }
+  }
+  res.json({
+    result: filteredUsers,
+  });
+});
