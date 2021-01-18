1babce79ee23dd9dd18a50fc624194cb746156c3
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-01-18 21:39:01 +0000

    Add module 4 lesson 2.5 exercise 1

---
+++ modulo-4/leccion-2.5/modulo-4-leccion-2.5-ejercicio-02-troceando-rick-morty/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const users = require("./data.json");
+
+// create app server
+const app = express();
+
+// set express middleware
+app.use(express.json());
+app.use(cors());
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+app.get("/users/:userId", (req, res) => {
+  console.log("Url params:", req.params);
+  console.log("Url param userId:", req.params.userId);
+
+  const user = users.find((user) => user.id === parseInt(req.params.userId));
+  console.log("Found user:", user);
+
+  if (user === undefined) {
+    res.json({ error: "user-not-found" });
+  } else {
+    res.json(user);
+  }
+});
+
+app.get("/users/:userId/:episodes", (req, res) => {
+  console.log("Url params:", req.params);
+  console.log("Url param userId:", req.params.userId);
+  console.log("Url param episodes:", req.params.episodes);
+
+  const episodes = users.find(
+    (user) => user.episode.length === req.params.episodes
+  );
+  console.log("Found user:", episodes);
+
+  if (episodes === undefined) {
+    res.json({ error: "episodes-not-found" });
+  } else {
+    res.json(episodes);
+  }
+});
