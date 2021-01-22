8e783effb0ae4230052ac288c8a97cf6373bc5dd
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2021-01-22 21:29:04 +0000

    Update exercise to review

--- modulo-4/leccion-3.2/modulo-4-leccion-3.2-ejercicio-02-filtra-año-premios/src/index.js
+++ modulo-4/leccion-3.2/modulo-4-leccion-3.2-ejercicio-02-filtra-año-premios/src/index.js
@@ -1,41 +1,51 @@
-// To review on pair programming
-const express = require("express");
-const cors = require("cors");
-const films = require("./films-data.json");
+// To review on pair programming. It only returns awards of 2003
+// const express = require("express");
+// const cors = require("cors");
+// const films = require("./films-data.json");
 
-// create app server
-const app = express();
+// // create app server
+// const app = express();
 
-// set express middlewares
-app.use(express.json());
-app.use(cors());
+// // set express middlewares
+// app.use(express.json());
+// app.use(cors());
 
-// set template engine middlewares
-app.set("view engine", "ejs");
+// // set template engine middlewares
+// app.set("view engine", "ejs");
 
-// init express aplication
-const serverPort = 3000;
-app.listen(serverPort, () => {
-  console.log(`App listening at http://localhost:${serverPort}`);
-});
+// // init express aplication
+// const serverPort = 3000;
+// app.listen(serverPort, () => {
+//   console.log(`App listening at http://localhost:${serverPort}`);
+// });
 
-// endpoints
+// // endpoints
 
-app.get("/es/film:filmId.html", (req, res) => {
-  // get film data
-  const filmData = films.find((film) => film.id === req.params.filmId);
-  console.log("film data", filmData);
+// app.get("/es/film:filmId.html", (req, res) => {
+//   // get film data
+//   const filmData = films.find((film) => film.id === req.params.filmId);
+//   console.log("film data", filmData);
 
-  // ensure data
-  filmData.title = filmData.title || "";
-  filmData.year = filmData.year || "";
-  filmData.director = filmData.director || "";
-  filmData.country = filmData.country || "";
+//   const awardsParams = {
+//     year: req.query.adwarsYear,
+//   };
 
-  // response with rendered template
-  if (filmData) {
-    res.render("pages/film", filmData);
-  } else {
-    res.render("pages/film-not-found");
-  }
-});
+//   const awardsData = filmData.awards.filter(
+//     (award) => award.year === awardsParams.year
+//   );
+//   console.log("awards data", awardsData);
+
+//   // ensure data
+//   filmData.title = filmData.title || "";
+//   filmData.year = filmData.year || "";
+//   filmData.director = filmData.director || "";
+//   filmData.country = filmData.country || "";
+//   filmData.awards = awardsData || [];
+
+//   // response with rendered template
+//   if (filmData) {
+//     res.render("pages/film", filmData);
+//   } else {
+//     res.render("pages/film-not-found");
+//   }
+// });
